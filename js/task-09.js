6017a4d6cac1b699c03d6b6848a71808ef03496d
Daniel Szul <amper00@gmail.com>
2024-02-16 15:24:40 +0100

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyColour = document.querySelector('.change-color');
+const spanText   = document.querySelector(`.color`);
+
+const changeBackgroundColor = () => {
+
+
+const randomColor = getRandomHexColor();
+document.body.style.backgroundColor = randomColor;
+spanText.textContent = randomColor;
+
+}
+
+
+
+
+
+
+
+
+bodyColour.addEventListener('click', changeBackgroundColor);
+
+
+
+
+
+
+
+
+
